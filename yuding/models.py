21418c8e97b03f2b31b29cdeb6b69e1b8b6d3d0c
allen <alleninfo001@gmail.com>
2021-08-24 11:33:50 +0800

    增加上传头像功能，没调试，有部分重置密码功能残留，也没调试

--- yuding/models.py
+++ yuding/models.py
@@ -4,9 +4,9 @@ from django.contrib.auth.models import AbstractUser
 class meetings(models.Model):
     name = models.CharField(max_length=200,null=True, verbose_name=u'会议室名称')
     people = models.CharField(max_length=100, null=True,verbose_name=u'容纳人数')
-    starttime = models.DateTimeField(null=True, verbose_name=u'开始时间')
-    endtime = models.DateTimeField(null=True,  verbose_name=u'结束时间')
-    createname = models.CharField(max_length=200, null=True, verbose_name=u'预约人')
+    starttime = models.DateTimeField(null=True, on_delete=models.SET_NULL, verbose_name=u'开始时间')
+    endtime = models.DateTimeField(null=True,  on_delete=models.SET_NULL, verbose_name=u'结束时间')
+    createname = models.CharField(max_length=200, null=True, on_delete=models.SET_NULL, verbose_name=u'预约人')
 
     class Meta:
         ordering = ['name']
